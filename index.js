1e4f96ac2d3b1c40dd3006e0664114f5755d256a
Tommaso Iovane <tommaso.iovane@gmail.com>
2019-06-23 12:16:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ global.document = new JSDOM(`
 express()
     // MiddleWare
     .use(bodyParser.json())
-    .use(bodyParser.urlencoded({extended: false}))
+    .use(bodyParser.urlencoded({extended: true}))
     .set('view engine', 'hjs')
     .use(express.static(__dirname + '/views'))
     .use(session({
@@ -45,31 +45,37 @@ express()
     })
     .post('/search', (req, res) => {
         let animeInput = req.body.animeInput
+
         let animeforce = req.body.animeforce
         let animehdita = req.body.animehdita
         let animeunity = req.body.animeunity
         let kuroneko = req.body.kuroneko
+        let hentaihaven = req.body.hentaihaven
 
         async.parallel({
             one: function (callback) {
-                let animeforceInput = animeInput.replace(' ', '+')
-                request(`https://ww1.animeforce.org/?s=${animeforceInput}`, (err, res, body) => {
-                    if (err) toast(err)
-                    let results = []
-                    let links = estraiLinksAnimeSearch(body)
-                    for (let i = 0; i < links.length; i++) {
-                        if (links[i].link.toString().includes('download-streaming')) {
-                            results.push(links[i])
+                if (animeforce) {
+                    let animeforceInput = animeInput.replace(' ', '+')
+                    request(`https://ww1.animeforce.org/?s=${animeforceInput}`, (err, res, body) => {
+                        if (err) toast(err)
+                        let results = []
+                        let links = estraiLinksAnimeSearch(body)
+                        for (let i = 0; i < links.length; i++) {
+                            if (links[i].link.toString().includes('download-streaming')) {
+                                results.push(links[i])
+                            }
                         }
-                    }
-                    let resultsUnique = []
-                    for (let i = 0; i < results.length; i++) {
-                        if (!results[i].text.includes('<img')) {
-                            resultsUnique.push(results[i])
+                        let resultsUnique = []
+                        for (let i = 0; i < results.length; i++) {
+                            if (!results[i].text.includes('<img')) {
+                                resultsUnique.push(results[i])
+                            }
                         }
-                    }
-                    callback(null, resultsUnique)
-                })
+                        callback(null, resultsUnique)
+                    })
+                } else {
+                    callback(null, null)
+                }
             },
             two: function (callback) {
                 callback(null, null)
@@ -78,11 +84,31 @@ express()
                 callback(null, null)
             },
             four: function (callback) {
+                if (kuroneko) {
+                    let kuronekoInput = animeInput.replace(' ', '+')
+                    request(`http://blog.libero.it/wp/animehousezone/?s=${kuronekoInput}&submit=Search`, (err, resp, body) => {
+                        if (err) toast(err)
+                        let links = estraiLinksAnimeSearch(body)
+                        let results = []
+                        let firstWord = kuronekoInput.toLowerCase().split('+')
+                        for (let i = 0; i < links.length; i++) {
+                            if (links[i].link.toString().includes('trama-streaming') && links[i].text.toLowerCase().includes(firstWord[0]) && !links[i].text.includes('/') ) {
+                                results.push(links[i])
+                            }
+                        }
+                        callback(null, results)
+                    })
+                } else {
+                    callback(null, null)
+                }
+            },
+            five: function (callback) {
                 callback(null, null)
             }
         }, function(err, results) {
             res.render('results', {
                 animeforce: results.one,
+                kuroneko: results.four,
                 name: animeInput
             })
         })
@@ -90,15 +116,13 @@ express()
     })
     .post('/animeforce', (req, res) => {
         let link = req.body.urlAnime
+        req.session.lastLink = link
         if (containsNumbers(link.toString().substring(20, link.toString().length))) {
-            request(link, (err, response, body) => {
-
-            })
+            res.redirect(link)
         } else {
             request(link, (err, response, body) => {
                 let links = estraiLinksAnimePage(body)
                 let unique = []
-
                 for (let i = 0; i < links.length; i++) {
                     for (let j = i + 1; j < links.length; j++) {
                         if (links[i].link == links[j].link) {
@@ -106,8 +130,6 @@ express()
                         }
                     }
                 }
-
-
                 let episodeLinks = []
                 for (let i = 0; i < links.length; i++) {
                     if (links[i].link.includes('.mp4') || links[i].link.includes('bit.ly')) {
@@ -120,11 +142,21 @@ express()
                 }
                 res.render('episodes', {
                     episodes: episodeLinks,
-                    message
+                    message,
+                    last: req.session.lastLink
                 })
             })
 
         }
+    })
+    .post('/kuroneko', (req, res) => {
+        let link = req.body.urlAnime
+        request(link, (err, resp, body) => {
+            if (err) console.log(err)
+            let links = estraiLinksAnimePage(body)
+            res.send(links)
+        })
+
     })
     .post('/download', (req, res) => {
         let link = req.body.urlAnime.replace('https', 'http')
@@ -252,6 +284,9 @@ express()
         }
     })
 
+    .get('*', (req, res) => {
+        res.render('404')
+    })
     .listen(process.env.PORT || 3000)
 
 
@@ -287,7 +322,21 @@ function estraiLinksAnimePage(rawHTML) {
     }
     return (urls)
 }
-
+function estraiLinksAnimePage4(params) {
+    let doc = document.createElement("html")
+    doc.innerHTML = rawHTML
+    let links = doc.getElementsByTagName("a")
+    let urls = []
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].getAttribute("href") != null) {
+            urls.push({
+                link: links[i].getAttribute("href"),
+                text: links[i].parentElement.parentElement.children[0].textContent
+            })
+        }
+    }
+    return (urls)
+}
 function containsNumbers(n) {
     return /\d/.test(n);
 }
